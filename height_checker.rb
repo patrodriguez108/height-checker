81f919be17415cad7e9818bc49a37aa31c24b51f
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-05-29 19:21:50 -0500

    Add javascript solution

--- height_checker.rb
+++ height_checker.rb
@@ -1,5 +1,11 @@
 def height_checker(heights)
+  # iterate through array
+    # if index zero, check if next student is same height
+    # if same height, move on to next student
+    # check if less than or equal to previous entry
 
+
+  # solution: clone array, in correct order; compare original and clone; return number of mismatches
 end
 
 height_checker([1,1,4,2,1,3])
